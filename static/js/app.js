902fd0a2796f8b4735cfd97fcbab92c8c3047e4e
F. Ulises Barroso <fyiaam@FYIAAM-MacBook-Pro.local>
2019-03-13 14:37:25 -0500

    populated table

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,18 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+console.log(data);
+
+data.forEach(function(ufoReport) {
+    console.log(ufoReport);
+    var row = tbody.append("tr");
+    Object.entries(ufoReport).forEach(function([key, value]) {
+      console.log(key, value);
+      
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+
